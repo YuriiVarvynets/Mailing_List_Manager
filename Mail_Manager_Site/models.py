32ec959d335cdae16e60d218e09b1db733738514
Yura Varvynets <varvynets@gmail.com>
2015-03-20 15:28:55 +0200

    table add

--- Mail_Manager_Site/models.py
+++ Mail_Manager_Site/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Property(models.Model):
     id = models.CharField(max_length=82, primary_key=True)
     Owner_Label_Name = models.CharField(max_length=100, null=True)
